53e51913f290bda7fc5685609aa268de6308c245
joseph shodunke <josephshodunke4@gmail.com>
2023-08-23 19:17:45 -0700

    Update views.py
    adding a page for invalid urls

--- avatar_api/avatar_api/views.py
+++ avatar_api/avatar_api/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+
+def invalid_endpoint(request):
+    return render(request, 'invalid_endpoint.html')
