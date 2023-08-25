77e83679540ed5410845c2b49ed3236219d6c951
joseph shodunke <josephshodunke4@gmail.com>
2023-08-25 02:15:13 -0700

    Update settings.py

--- avatar_api/avatar_api/settings.py
+++ avatar_api/avatar_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mu+^%#56b3!(@hja59j5yeg%)dj(hp9y9wss96qgx=2=#(x*t2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
