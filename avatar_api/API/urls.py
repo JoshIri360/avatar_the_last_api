cca8d125ef458cc488b6bc336a1d80231c94e833
joseph shodunke <josephshodunke4@gmail.com>
2023-08-29 05:21:45 -0700

    Update urls.py

--- avatar_api/API/urls.py
+++ avatar_api/API/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path , include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('Characters/Air-Nation/', views.Air_data, name="Air_Data"),
-    path('Characters/Fire-Nation/', views.Fire_data, name="fire_Data"),
-    path('Characters/Water-Nation/', views.Water_data, name="Water_Data"),
-    path('Characters/Earth-Nation/', views.Earth_data, name="Earth_Data"),
-    path('Characters/', views.All_characters, name="All_Data"),
-]
+    path('api/Characters/Air-Nation/', views.Air_data, name="Air_Data"),
+    path('api/Characters/Fire-Nation/', views.Fire_data, name="fire_Data"),
+    path('api/Characters/Water-Nation/', views.Water_data, name="Water_Data"),
+    path('api/Characters/Earth-Nation/', views.Earth_data, name="Earth_Data"),
+    
 
+    path('api/Characters/Air-Nation/<int:pk>/', views.Air_data, name="Air_Data"),
+    path('api/Characters/Fire-Nation/<int:pk>/', views.Fire_data, name="fire_Data"),
+    path('api/Characters/Water-Nation/<int:pk>/', views.Water_data, name="Water_Data"),
+    path('api/Characters/Earth-Nation/<int:pk>/', views.Earth_data, name="Earth_Data"),
+    
+    # URL pattern that works without 'pk' (no trailing slash)
+    path('api/Characters/', views.All_characters, name="All_Data"),
+    
+    # URL pattern that works with 'pk' (no trailing slash)
+    path('api/Characters/<int:pk>', views.All_characters, name="All_Data"),
+]
